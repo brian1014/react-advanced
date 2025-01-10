86c6e7620434166d10b7a74096ed5f0521aad490
Brian Alvarez <brianalvarez2000@gmail.com>
2025-01-10 10:22:01 -0500

    fix packange react router dom

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router";
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
 import logo from "../assets/react.svg";
 
 export function Navigation() {
